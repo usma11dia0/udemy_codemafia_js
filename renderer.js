68540093fba0e05a536e0823d0d5092dbb44d10e
usma11dia0 <usma11dia0@gmail.com>
2022-05-07 21:10:01 +0900

    add patch part1

--- renderer.js
+++ renderer.js
@@ -1,3 +1,5 @@
+import { nodeOps } from './nodeOps.js';
+
 function createVNode(type='', props = {}, children ='') {
     return{
         type,
@@ -8,6 +10,14 @@ function createVNode(type='', props = {}, children ='') {
 
 function patch(n1, n2, container) {
     console.log(n1, n2);
+    let el;
+    if(n1.type !== n2.type){
+        el = nodeOps.create(n2.type);
+        nodeOps.append(container, el);
+    }
+    if(n1.children !== n2.children){
+        nodeOps.html(el, n2.children);
+    }
 }
 
 export { createVNode, patch};
